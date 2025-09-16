30ae27203b7b8622e22a5ca3440089a369e296db
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-09-16 19:14:11 +0200

    feat(exercises): add exercises table with draggable rows and loading state

---
+++ components/trainings/exercises/exercises-table/exercises-table.js
@@ -0,0 +1,66 @@
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+} from "@/components/ui/table";
+
+import ExercisesTableRow from "./exercises-table-row";
+import Spinner from "@/components/ui/loading-dots";
+
+export default function ExercisesTable({ columns, data, isLoading = false }) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="w-[30%] h-[70vh] overflow-hidden rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {isLoading ? (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="text-center py-6">
+                <Spinner className="justify-center" />
+              </TableCell>
+            </TableRow>
+          ) : table.getRowModel().rows.length ? (
+            table
+              .getRowModel()
+              .rows.map((row) => <ExercisesTableRow key={row.id} row={row} />)
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
