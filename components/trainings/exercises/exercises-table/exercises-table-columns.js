72c0890277b921e1efd148d50de25720b03f3862
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-09-19 14:53:30 +0200

    feat(exercises): add actions column with edit/delete menu in table

--- components/trainings/exercises/exercises-table/exercises-table-columns.js
+++ components/trainings/exercises/exercises-table/exercises-table-columns.js
@@ -1,14 +1,23 @@
-export const ExercisesTableColumns = [
+"use client";
+
+import ActionsCell from "./actions-cell";
+
+export const createExercisesTableColumns = ({ onEditOpen, onDelete } = {}) => [
+  { accessorKey: "title", header: "Title" },
+  { accessorKey: "muscle_group", header: "Muscle Group" },
+  { accessorKey: "exercise_type", header: "Exercise Type" },
   {
-    accessorKey: "title",
-    header: "Title",
-  },
-  {
-    accessorKey: "muscle_group",
-    header: "Muscle Group",
-  },
-  {
-    accessorKey: "exercise_type",
-    header: "Exercise Type",
+    id: "actions",
+    header: "",
+    cell: ({ row }) => {
+      const exercise = row.original;
+      return (
+        <ActionsCell
+          exercise={exercise}
+          onEditOpen={onEditOpen}
+          onDelete={onDelete}
+        />
+      );
+    },
   },
 ];
