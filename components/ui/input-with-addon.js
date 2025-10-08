8d5bf104b60c66379c1d83719b0c2d61e80f7534
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-10-08 19:43:12 +0200

    refactor(ui): replace Input with NumericInput in InputWithAddon for numeric support

--- components/ui/input-with-addon.js
+++ components/ui/input-with-addon.js
@@ -1,11 +1,10 @@
-import * as React from "react";
-import { Input } from "./input"; // твой компонент Input
 import { cn } from "@/lib/utils";
+import { NumericInput } from "./numeric-input";
 
 function InputWithAddon({ addon, className, ...props }) {
   return (
     <div className={cn("relative w-24 min-w-0", className)}>
-      <Input {...props} className={cn("pr-10 h-8", props.className)} />
+      <NumericInput {...props} className={cn("pr-10 h-8", props.className)} />
       <span className="absolute inset-y-0 right-2 flex items-center text-sm text-muted-foreground pointer-events-none">
         {addon}
       </span>
