1765d040f06d9b80e06b40e368d9fb75b1a73cf5
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-10-31 18:23:24 +0100

    feat(meals): add My Favorite Meals page for user favorites

---
+++ app/meals/favorites/page.js
@@ -0,0 +1,16 @@
+"use server";
+
+import MealsGrid from "@/components/meals/meals-grid";
+import { getCurrentUser } from "@/lib/auth";
+import { getFavoriteMealsByUser } from "@/lib/repository/meals";
+
+export default async function MyFavoriteMealsPage() {
+  const user = await getCurrentUser();
+  const meals = await getFavoriteMealsByUser(user?.id);
+
+  return (
+    <main className="flex-1 pt-[92px] p-6 bg-gray-50">
+      <MealsGrid meals={meals} />
+    </main>
+  );
+}
