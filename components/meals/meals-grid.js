510a1ff70d4db2fc1397ac2945e6e8a1bc5037f5
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-11-06 21:58:07 +0100

    feat(meals): pass currentUserId to MealsGrid and MealItem to enable conditional delete actions

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -7,7 +7,7 @@ import { Button } from "../ui/button";
 import MealItem from "./meal-item";
 import MealsPagination from "./meals-pagination";
 
-export default function MealsGrid({ meals }) {
+export default function MealsGrid({ meals, currentUserId }) {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 8;
   const totalPages = Math.ceil(meals.length / itemsPerPage);
@@ -24,7 +24,7 @@ export default function MealsGrid({ meals }) {
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 list-none p-0 m-0">
           {currentMeals.map((meal) => (
             <li key={meal.id}>
-              <MealItem {...meal} />
+              <MealItem {...meal} currentUserId={currentUserId} />
             </li>
           ))}
         </ul>
