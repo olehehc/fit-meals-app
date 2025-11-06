98932ca697c939ab707518cf62882e45102794ed
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-11-06 21:58:52 +0100

    refactor(auth): destructure 'from' param in SignInPage for cleaner code

--- app/auth/sign-in/page.js
+++ app/auth/sign-in/page.js
@@ -1,7 +1,7 @@
 import SignInCard from "@/components/auth/sign-in-card";
 
 export default async function SignInPage({ searchParams }) {
-  const from = await searchParams.from;
+  const { from } = await searchParams;
 
   return (
     <main className="flex-1 flex items-center justify-center pt-[92px] p-6 bg-gray-50">
