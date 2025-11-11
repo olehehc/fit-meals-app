0001f7cb46d02ccbac750d1a396bb804d3a66d87
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-11-11 10:33:35 +0100

    feat(header): add responsive MobileNavigation for smaller screens and adjust layout

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,16 +1,22 @@
 import Link from "next/link";
 import { getCurrentUser } from "@/lib/auth";
 import Navigation from "./navigation";
+import MobileNavigation from "./mobile-navigation";
 
 export default async function MainHeader() {
   const user = await getCurrentUser();
 
   return (
-    <header className="flex p-4 fixed top-0 left-0 w-full z-50 bg-black justify-between">
+    <header className="flex p-4 fixed top-0 left-0 w-full z-50 bg-black justify-between items-center">
       <Link href="/" className="text-white text-xl font-bold">
         💪 StayFit
       </Link>
-      <Navigation user={user} />
+      <div className="hidden md:flex">
+        <Navigation user={user} />
+      </div>
+      <div className="md:hidden">
+        <MobileNavigation user={user} />
+      </div>
     </header>
   );
 }
