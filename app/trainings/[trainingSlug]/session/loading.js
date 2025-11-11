c89ad01752e34ec143e1672af37a3cade5a9404e
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-11-11 16:59:27 +0100

    feat(loading): add Loading UI for /trainings/[trainingSlug]/session route with centered LoadingDots indicator

---
+++ app/trainings/[trainingSlug]/session/loading.js
@@ -0,0 +1,11 @@
+"use client";
+
+import LoadingDots from "@/components/ui/loading-dots";
+
+export default function Loading() {
+  return (
+    <main className="flex flex-col items-center justify-start pt-[92px] p-6 bg-gray-50 flex-1">
+      <LoadingDots />
+    </main>
+  );
+}
